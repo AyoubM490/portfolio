7dede8644dfbacb37ca3235f882bc26485057406
ayubch <ayoub.chahir96@gmail.com>
2022-01-24 13:41:37 +0100

    Add ESLint config file

--- script.js
+++ script.js
@@ -1,113 +1,112 @@
-let body = document.querySelector("body");
-let menuHamb = document.querySelector("[alt *= 'menu']");
-let logo = document.querySelector(".logo");
-let nav = document.querySelector("nav");
-let headline = document.querySelector(".headline");
-let header = document.querySelector("header");
-let portfolioSection = document.querySelector("#portfolio");
-let aboutSection = document.querySelector("#about");
-let contactSection = document.querySelector("#contact");
-let portfolioNav = document.querySelector(".portfolio");
-let aboutNav = document.querySelector(".about");
-let contactNav = document.querySelector(".contact");
+const menuHamb = document.querySelector("[alt *= 'menu']");
+const logo = document.querySelector('.logo');
+const nav = document.querySelector('nav');
+const headline = document.querySelector('.headline');
+const header = document.querySelector('header');
+const portfolioSection = document.querySelector('#portfolio');
+const aboutSection = document.querySelector('#about');
+const contactSection = document.querySelector('#contact');
+const portfolioNav = document.querySelector('.portfolio');
+const aboutNav = document.querySelector('.about');
+const contactNav = document.querySelector('.contact');
 
 function styleMenu(elem) {
-  elem.style.color = "#fff";
-  elem.style.fontWeight = "600";
-  elem.style.fontStyle = "normal";
-  elem.style.fontSize = "32px";
-  elem.style.lineHeight = "44px";
-  elem.style.textDecoration = "none";
-  elem.style.marginBottom = "40px";
+  elem.style.color = '#fff';
+  elem.style.fontWeight = '600';
+  elem.style.fontStyle = 'normal';
+  elem.style.fontSize = '32px';
+  elem.style.lineHeight = '44px';
+  elem.style.textDecoration = 'none';
+  elem.style.marginBottom = '40px';
 }
 
-let menuClose = document.createElement("img");
+const menuClose = document.createElement('img');
 menuClose.style.zIndex = 998;
-menuClose.style.position = "absolute";
-menuClose.style.top = "34px";
-menuClose.style.left = "325px";
-menuClose.src = "./img/Close-Button.svg";
+menuClose.style.position = 'absolute';
+menuClose.style.top = '34px';
+menuClose.style.left = '325px';
+menuClose.src = './img/Close-Button.svg';
 
-menuHamb.addEventListener("click", function () {
-  headline.style.display = "none";
-  portfolioSection.style.display = "none";
-  aboutSection.style.display = "none";
-  contactSection.style.display = "none";
-  logo.style.display = "none";
-  header.style.backgroundColor = "#6070FF";
+menuHamb.addEventListener('click', () => {
+  headline.style.display = 'none';
+  portfolioSection.style.display = 'none';
+  aboutSection.style.display = 'none';
+  contactSection.style.display = 'none';
+  logo.style.display = 'none';
+  header.style.backgroundColor = '#6070FF';
   nav.appendChild(menuClose);
-  nav.style.display = "flex";
-  nav.style.flexDirection = "column";
-  nav.style.alignItems = "start";
-  nav.style.marginTop = "115px";
-  portfolioNav.classList.remove("desktop");
-  aboutNav.classList.remove("desktop");
-  contactNav.classList.remove("desktop");
+  nav.style.display = 'flex';
+  nav.style.flexDirection = 'column';
+  nav.style.alignItems = 'start';
+  nav.style.marginTop = '115px';
+  portfolioNav.classList.remove('desktop');
+  aboutNav.classList.remove('desktop');
+  contactNav.classList.remove('desktop');
   styleMenu(portfolioNav);
   styleMenu(aboutNav);
   styleMenu(contactNav);
 });
 
-menuClose.addEventListener("click", function () {
-  headline.style.display = "block";
-  portfolioSection.style.display = "block";
-  aboutSection.style.display = "block";
-  contactSection.style.display = "block";
-  logo.style.display = "flex";
-  nav.style.flexDirection = "row";
-  header.style.backgroundColor = "#fff";
-  nav.style.display = "flex";
-  nav.style.alignItems = "center";
-  nav.style.marginTop = "0px";
-  portfolioNav.classList.add("desktop");
-  aboutNav.classList.add("desktop");
-  contactNav.classList.add("desktop");
+menuClose.addEventListener('click', () => {
+  headline.style.display = 'block';
+  portfolioSection.style.display = 'block';
+  aboutSection.style.display = 'block';
+  contactSection.style.display = 'block';
+  logo.style.display = 'flex';
+  nav.style.flexDirection = 'row';
+  header.style.backgroundColor = '#fff';
+  nav.style.display = 'flex';
+  nav.style.alignItems = 'center';
+  nav.style.marginTop = '0px';
+  portfolioNav.classList.add('desktop');
+  aboutNav.classList.add('desktop');
+  contactNav.classList.add('desktop');
 });
 
-portfolioNav.addEventListener("click", function () {
-  headline.style.display = "block";
-  portfolioSection.style.display = "block";
-  aboutSection.style.display = "block";
-  contactSection.style.display = "block";
-  logo.style.display = "flex";
-  nav.style.flexDirection = "row";
-  header.style.backgroundColor = "#fff";
-  nav.style.display = "flex";
-  nav.style.alignItems = "center";
-  nav.style.marginTop = "0px";
-  portfolioNav.classList.add("desktop");
-  aboutNav.classList.add("desktop");
-  contactNav.classList.add("desktop");
+portfolioNav.addEventListener('click', () => {
+  headline.style.display = 'block';
+  portfolioSection.style.display = 'block';
+  aboutSection.style.display = 'block';
+  contactSection.style.display = 'block';
+  logo.style.display = 'flex';
+  nav.style.flexDirection = 'row';
+  header.style.backgroundColor = '#fff';
+  nav.style.display = 'flex';
+  nav.style.alignItems = 'center';
+  nav.style.marginTop = '0px';
+  portfolioNav.classList.add('desktop');
+  aboutNav.classList.add('desktop');
+  contactNav.classList.add('desktop');
 });
 
-aboutNav.addEventListener("click", function () {
-  headline.style.display = "block";
-  portfolioSection.style.display = "block";
-  aboutSection.style.display = "block";
-  contactSection.style.display = "block";
-  logo.style.display = "flex";
-  nav.style.flexDirection = "row";
-  header.style.backgroundColor = "#fff";
-  nav.style.display = "flex";
-  nav.style.alignItems = "center";
-  nav.style.marginTop = "0px";
-  portfolioNav.classList.add("desktop");
-  aboutNav.classList.add("desktop");
-  contactNav.classList.add("desktop");
+aboutNav.addEventListener('click', () => {
+  headline.style.display = 'block';
+  portfolioSection.style.display = 'block';
+  aboutSection.style.display = 'block';
+  contactSection.style.display = 'block';
+  logo.style.display = 'flex';
+  nav.style.flexDirection = 'row';
+  header.style.backgroundColor = '#fff';
+  nav.style.display = 'flex';
+  nav.style.alignItems = 'center';
+  nav.style.marginTop = '0px';
+  portfolioNav.classList.add('desktop');
+  aboutNav.classList.add('desktop');
+  contactNav.classList.add('desktop');
 });
 
-contactNav.addEventListener("click", function () {
-  headline.style.display = "block";
-  portfolioSection.style.display = "block";
-  aboutSection.style.display = "block";
-  contactSection.style.display = "block";
-  logo.style.display = "flex";
-  nav.style.flexDirection = "row";
-  header.style.backgroundColor = "#fff";
-  nav.style.display = "flex";
-  nav.style.alignItems = "center";
-  nav.style.marginTop = "0px";
-  portfolioNav.classList.add("desktop");
-  aboutNav.classList.add("desktop");
-  contactNav.classList.add("desktop");
+contactNav.addEventListener('click', () => {
+  headline.style.display = 'block';
+  portfolioSection.style.display = 'block';
+  aboutSection.style.display = 'block';
+  contactSection.style.display = 'block';
+  logo.style.display = 'flex';
+  nav.style.flexDirection = 'row';
+  header.style.backgroundColor = '#fff';
+  nav.style.display = 'flex';
+  nav.style.alignItems = 'center';
+  nav.style.marginTop = '0px';
+  portfolioNav.classList.add('desktop');
+  aboutNav.classList.add('desktop');
+  contactNav.classList.add('desktop');
 });
