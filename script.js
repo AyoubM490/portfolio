eaf5cf8bf334c1147a4b2dc764b564efbc5ae75d
ayubch <ayoub.chahir96@gmail.com>
2022-01-25 20:23:44 +0100

    Add contact form validation

--- script.js
+++ script.js
@@ -16,6 +16,9 @@ const seeProjectBtnDesks = document.querySelectorAll('.see-modal-desktop');
 const modalDesk = document.querySelector('.modal-desktop');
 const leftBlockModal = document.querySelector('.left-block-modal');
 const closeBtnDesk = document.querySelector('.close-btn-desktop');
+const form = document.getElementById('contact-form');
+const nameDesk = form.elements['name-desktop'];
+const emailDesk = form.elements['email-desktop'];
 
 function styleMenu(elem) {
   elem.style.color = '#fff';
@@ -284,4 +287,65 @@ closeBtnDesk.addEventListener('click', () => {
   logo.style.display = 'block';
   nav.style.display = 'flex';
   modalDesk.style.display = 'none';
+});
+
+// show a message with a type of the input
+function showMessage(input, message, type) {
+  // get the small element and set the message
+  const msg = input.parentNode.querySelector('small');
+  msg.innerText = message;
+  // update the class for the input
+  input.className = type ? 'success' : 'error';
+  return type;
+}
+
+function showError(input, message) {
+  return showMessage(input, message, false);
+}
+
+function showSuccess(input) {
+  return showMessage(input, '', true);
+}
+
+function hasValue(input, message) {
+  if (input.value.trim() === '') {
+    return showError(input, message);
+  }
+  return showSuccess(input);
+}
+
+function validateEmail(input, requiredMsg, invalidMsg) {
+  // check if the value is not empty
+  if (!hasValue(input, requiredMsg)) {
+    return false;
+  }
+  // validate email format
+  const emailRegex = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
+
+  const email = input.value.trim();
+  if (!emailRegex.test(email)) {
+    return showError(input, invalidMsg);
+  }
+  return true;
+}
+
+const NAME_REQUIRED = 'Please enter your name';
+const EMAIL_REQUIRED = 'Please enter your email';
+const EMAIL_INVALID = 'Please enter a correct email address format';
+
+form.addEventListener('submit', (event) => {
+  // stop form submission
+  event.preventDefault();
+
+  // validate the form
+  const nameDeskValid = hasValue(nameDesk, NAME_REQUIRED);
+  const emailDeskValid = validateEmail(
+    emailDesk,
+    EMAIL_REQUIRED,
+    EMAIL_INVALID,
+  );
+  // if valid, submit the form.
+  if (nameDeskValid && emailDeskValid) {
+    form.submit();
+  }
 });
